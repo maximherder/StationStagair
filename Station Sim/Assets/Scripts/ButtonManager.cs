2adf90865670a92dbc6ea98991d39727a7a2a647
maximherder <79004750+maximherder@users.noreply.github.com>
2023-01-25 10:41:39 +0100

    cleanup

---
+++ Station Sim/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    private GameObject controlsPanel;
+    private bool _controlPanelActive;
+    public List<GameObject> PuzzlePieces;
+
+    void Awake()
+    {
+        controlsPanel = GameObject.Find("ControlsPanelExposed");
+        controlsPanel.SetActive(false);
+        _controlPanelActive = false;
+    }
+
+
+    public void CanBeBuilt()
+    {
+        
+    }
+
+
+    public void ToggleControlsPanel()
+    {
+        _controlPanelActive = !_controlPanelActive;
+
+        if (_controlPanelActive)
+        {
+            controlsPanel.SetActive(true);
+        }
+        if (!_controlPanelActive)
+        {
+            controlsPanel.SetActive(false);
+        }
+    }
+
+}
---
+++ Station Sim/Assets/Scripts/PieceValueScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceValueScript : MonoBehaviour
+{
+    public float BuildingTime;
+    public bool IsBuilt;
+    public bool IsInProgress;
+}
